44486b7f83e5e0b2c7d560c85f65bf5bc8f0ff0a
ahmadEssam77 <ahmad77essam@gmail.com>
2025-10-29 22:24:12 +0300

    Added Procfile and runtime.txt for Render deployment

--- App.py
+++ App.py
@@ -1,13 +1,15 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return '''
-        <h1>Hello Ahmad!</h1>
-        <p>Here is the link you need: <a target="_blank" href="https://www.google.com">Click here</a></p>
+        <h1>Hello Team!</h1>
+        <p>Here is the link you need: <a target="_blank" href="https://google.com">Click here</a></p>
     '''
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
